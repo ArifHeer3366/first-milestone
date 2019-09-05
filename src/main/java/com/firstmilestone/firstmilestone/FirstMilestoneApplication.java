946e90a50f733b8e74362700fc8a420a311c7671
Arif Heer <arif.heer3366@gmail.com>
2019-09-05 12:55:27 +0500

    m1 Added for version-1

---
+++ src/main/java/com/firstmilestone/firstmilestone/FirstMilestoneApplication.java
@@ -0,0 +1,13 @@
+package com.firstmilestone.firstmilestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMilestoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstMilestoneApplication.class, args);
+    }
+
+}
